36b1b408707f82eca29c650007bbe5b07ea0be82
jensculrich <jensj27@gmail.com>
2022-05-24 11:23:12 -0700

    initialized connection and created new file with title and sample code formatting

---
+++ Rfile_data_cleaning_and_exploration.R
@@ -0,0 +1,9 @@
+### Floral resource data cleaning and exploration
+### started on May 24, 2022,
+### Jens U and Erin M
+
+## read data
+# quadrat floral resource data from 2021
+quadrat_data <- read.csv("./_.csv")
+# hectare floral resource data from 2021
+hectare_data <- read.csv("./_.csv")
