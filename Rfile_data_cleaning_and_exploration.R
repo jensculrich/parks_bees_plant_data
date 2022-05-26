91c3ab450b73d3231073b48072d9c3b4097d3904
emchugh1 <106178507+emchugh1@users.noreply.github.com>
2022-05-26 13:30:53 -0700

    New code

--- Rfile_data_cleaning_and_exploration.R
+++ Rfile_data_cleaning_and_exploration.R
@@ -14,9 +14,10 @@ library(ggplot2)
 ## read data
 # quadrat floral resource data from 2021
 quadrat_data <- read.csv("./quadrat_flower_surveys_2021.csv")
+ 
 # hectare floral resource data from 2021
 hectare_data <- read.csv("./hectare_floral_surveys_2021.csv")
-
+hectare_data<-hectare_data[!is.na(hectare_data$SPECIES),]
 
 ## Clean data
 # First, add whether each site is mowed or unmowed. Unmowed=0 and mowed=1.
@@ -25,6 +26,12 @@ quadrat_data %>%
   mutate(TREATMENT=ifelse(SITE=="Rupert"|SITE=="Slocan"|SITE=="Bobolink"|SITE=="Gordon"|SITE=="Moberly"|SITE=="Winona"|SITE=="Balaclava"|SITE=="Quilchena"|SITE=="Kensington", "1", "0"))->quadrat
 
 str(quadrat)
+levels(quadrat$SITE)
+
+quadrat$SITE[which(quadrat$SITE=="Memorial west")]<-"Memorial West"
+quadrat$SITE[which(quadrat$SITE=="South Memorial")]<-"Memorial South"
+quadrat$SITE<-as.factor(quadrat$SITE)
+levels(quadrat$SITE)
 
 # Next, add treatment column to the hectare data.
 hectare_data %>% 
@@ -32,34 +39,23 @@ hectare_data %>%
 
 str(hectare)
 
-# Now, make a table showing total floral resources and floral diversity by each park
-# First, merge the two data frames to create one dataframe
+hectare$SITE[which(hectare$SITE=="South Memorial")]<-"Memorial South"
 
-hectare$NUM_FLORAL_UNITS<-as.numeric(hectare$NUM_FLORAL_UNITS)
-quadrat$NUM_FLORAL_UNITS<-as.numeric(quadrat$NUM_FLORAL_UNITS)
-data_total<-bind_rows(hectare, quadrat)
+hectare$SITE[which(hectare$SITE=="West Memorial")]<-"Memorial West"
 
-# Group by site to make things easier!
-data_total %>% 
-  group_by(SITE)->data_total_sorted
+hectare$SITE[which(hectare$SITE=="Memorial west")]<-"Memorial West"
 
-# Hm, it would appear that Memorial South, Memorial West, and Locarno are written in a few different ways! This code should fix things:
+hectare$SITE[which(hectare$SITE=="Lucarno")]<-"Locarno"
 
-data_total_sorted$SITE[which(data_total_sorted$SITE=="South Memorial")]<-"Memorial South"
+hectare$SITE<-as.factor(hectare$SITE)
+levels(hectare$SITE)
 
-data_total_sorted$SITE[which(data_total_sorted$SITE=="West Memorial")]<-"Memorial West"
-
-data_total_sorted$SITE[which(data_total_sorted$SITE=="Memorial west")]<-"Memorial West"
-
-data_total_sorted$SITE[which(data_total_sorted$SITE=="Lucarno")]<-"Locarno"
-
-# Double-check that the code worked!
-data_total_sorted$SITE<-as.factor(data_total_sorted$SITE)
-levels(data_total_sorted$SITE)
-str(data_total_sorted)
 # It worked!
 # Time for some more cleaning. I'm going to check out whether these species have been spelled properly, or if there are actually duplicates
-levels(data_total_sorted$SPECIES)
+quadrat$SPECIES<-as.factor(quadrat$SPECIES)
+levels(quadrat$SPECIES)
+hectare$SPECIES<-as.factor(hectare$SPECIES)
+levels(hectare$SPECIES)
 # duplicates to change: change "acer capestre"-> acer campestre; "achillea milleflolium"-> achillea milleflolium, "Crateaugus douglasii"->"Crateagus douglasii"; "Crateaugous monogyna"->"Crateagus monogyna"; "Magnolia X soulangeana"->"Magnolia x soulangeana"; "Malus sp"->"Malus sp."; "PLA SOL"->???, "Rosa sp"->"Rosa sp.", "Solanum dulcmara"->"Solanum dulcamara"
 
 ## Make a new dataframe with summary stats
@@ -67,51 +63,103 @@ levels(data_total_sorted$SPECIES)
 # First, use lubridate to show R how to read the dates
 
 library(lubridate)
-data_total_sorted$DATE<-as.Date(data_total_sorted$DATE, "%m.%d.%Y")
+quadrat$DATE<-as.Date(quadrat$DATE, "%m.%d.%Y")
+hectare$DATE<-as.Date(hectare$DATE, "%m.%d.%Y")
 
 # Next, sort original dataframe by month-- this will put the month into the date column, allowing me to group data by month.
-data_total_sorted$DATE<-month(data_total_sorted$DATE)
+quadrat$MONTH<-month(quadrat$DATE)
+hectare$MONTH<-month(hectare$DATE)
 
 # Okay time to make the new dataframe
 # First, make sure R is reading the "species" and "date" columns as factors
 
-data_total_sorted$SPECIES<-as.factor(data_total_sorted$SPECIES)
+quadrat$SPECIES<-as.factor(quadrat$SPECIES)
+quadrat$DATE<-as.factor(quadrat$DATE)
+quadrat$NUM_FLORAL_UNITS<-as.numeric(quadrat$NUM_FLORAL_UNITS)
 
-data_total_sorted$DATE<-as.factor(data_total_sorted$DATE)
+hectare$SPECIES<-as.factor(hectare$SPECIES)
+hectare$DATE<-as.factor(hectare$DATE)
+hectare$NUM_FLORAL_UNITS<-as.numeric(hectare$NUM_FLORAL_UNITS)
 
 # Make a new dataframe with total species and abudance counts (i.e. total number of species and number of floral units observed for each park)
 # Changed abundances to per square metre aka divide by 20
-data_total_sorted %>% 
+quadrat %>% 
   na.omit() %>% 
   group_by(SITE, TREATMENT) %>% 
-  summarise(TOT_ABUND=((sum(NUM_FLORAL_UNITS))/20),
+  summarise(
+            TOT_ABUND=((sum(NUM_FLORAL_UNITS))/20),
             TOT_RICHNESS=n_distinct(SPECIES),
-            MAY_TOT_ABUND=((sum(subset(NUM_FLORAL_UNITS, DATE=="5")))/20),
-            MAY_TOT_RICH=n_distinct(subset(SPECIES, DATE=="5")),
-            JUN_TOT_ABUND=((sum(subset(NUM_FLORAL_UNITS, DATE=="6")))/20),
-            JUN_TOT_RICH=n_distinct(subset(SPECIES, DATE=="6")),
-            JUL_TOT_ABUND=((sum(subset(NUM_FLORAL_UNITS, DATE=="7")))/20), 
-            JUL_TOT_RICH=n_distinct(subset(SPECIES, DATE=="7")),
-            AUG_TOT_ABUND=((sum(subset(NUM_FLORAL_UNITS, DATE=="8")))/20),
-            AUG_TOT_RICH=n_distinct(subset(SPECIES, DATE=="8")),
-            MEAN_ABUND=mean(c(subset(NUM_FLORAL_UNITS, DATE=="5"), subset(NUM_FLORAL_UNITS, DATE=="6"), subset(NUM_FLORAL_UNITS, DATE=="7"), subset(NUM_FLORAL_UNITS, DATE=="8"))))->TOT_COUNTS
+            MAY_TOT_ABUND=((sum(subset(NUM_FLORAL_UNITS, MONTH=="5")))/20),
+            MAY_TOT_RICH=n_distinct(subset(SPECIES, MONTH=="5")),
+            JUN_TOT_ABUND=((sum(subset(NUM_FLORAL_UNITS, MONTH=="6")))/20),
+            JUN_TOT_RICH=n_distinct(subset(SPECIES, MONTH=="6")),
+            JUL_TOT_ABUND=((sum(subset(NUM_FLORAL_UNITS, MONTH=="7")))/20), 
+            JUL_TOT_RICH=n_distinct(subset(SPECIES, MONTH=="7")),
+            AUG_TOT_ABUND=((sum(subset(NUM_FLORAL_UNITS, MONTH=="8")))/20),
+            AUG_TOT_RICH=n_distinct(subset(SPECIES, MONTH=="8")),
+            MEAN_ABUND=mean(c(subset(NUM_FLORAL_UNITS, MONTH=="5"), subset(NUM_FLORAL_UNITS, MONTH=="6"), subset(NUM_FLORAL_UNITS, MONTH=="7"), subset(NUM_FLORAL_UNITS, MONTH=="8"))),
+            MEAN_RICH=mean(c(MAY_TOT_RICH, JUN_TOT_RICH, JUL_TOT_RICH, AUG_TOT_RICH)))->TOT_COUNTS_QUAD
 
-# Come back to this to add in mean richness!!!
+
+
+# Same table, but for hectare data
+levels(hectare$SITE)
+hectare %>% 
+  na.omit() %>% 
+  group_by(SITE, TREATMENT) %>% 
+  summarise(
+            TOT_ABUND=((sum(NUM_FLORAL_UNITS))/20),
+            TOT_RICHNESS=n_distinct(SPECIES),
+            MAY_TOT_ABUND=((sum(subset(NUM_FLORAL_UNITS, MONTH=="5")))/20),
+            MAY_TOT_RICH=n_distinct(subset(SPECIES, MONTH=="5")),
+            JUN_TOT_ABUND=((sum(subset(NUM_FLORAL_UNITS, MONTH=="6")))/20),
+            JUN_TOT_RICH=n_distinct(subset(SPECIES, MONTH=="6")),
+            JUL_TOT_ABUND=((sum(subset(NUM_FLORAL_UNITS, MONTH=="7")))/20), 
+            JUL_TOT_RICH=n_distinct(subset(SPECIES, MONTH=="7")),
+            AUG_TOT_ABUND=((sum(subset(NUM_FLORAL_UNITS, MONTH=="8")))/20),
+            AUG_TOT_RICH=n_distinct(subset(SPECIES, MONTH=="8")),
+            MEAN_ABUND=mean(c(subset(NUM_FLORAL_UNITS, MONTH=="5"), subset(NUM_FLORAL_UNITS, MONTH=="6"), subset(NUM_FLORAL_UNITS, MONTH=="7"), subset(NUM_FLORAL_UNITS, MONTH=="8"))),
+            MEAN_RICH=mean(c(MAY_TOT_RICH, JUN_TOT_RICH, JUL_TOT_RICH, AUG_TOT_RICH)))->TOT_COUNTS_HECTARE
+            
+
+# I need to think a bit more about this-- is the mean for each month just the mean of all observations? I think so, and if so, this is pretty meaningless data
 
 ## Plot data
-# First, how do mean abundances vary between mowed and unmowed parks?
-ggplot(TOT_COUNTS, aes(x=TREATMENT, y=MEAN_ABUND))+
-  geom_boxplot(stat="boxplot")
+# First, how do total abundances vary between mowed and unmowed parks?
+ggplot(TOT_COUNTS_QUAD, aes(x=TREATMENT, y=TOT_ABUND))+
+         geom_boxplot(stat="boxplot")+
+         geom_point()
 
-## Statistical tests
-# time to try a t-test just for fun
+t.test(TOT_ABUND~TREATMENT, data=TOT_COUNTS_QUAD, var.equal=FALSE)
 
-t.test(TOT_RICHNESS~TREATMENT, data=TOT_COUNTS, var.equal=FALSE)
+# Next, how do total species richnesses vary between mowed and unmowed parks?
+ggplot(TOT_COUNTS_QUAD, aes(x=TREATMENT, y=TOT_RICHNESS))+
+  geom_boxplot(stat="boxplot")+
+  geom_point()
 
-# The mean total species richness per group is 27.67 for unmowed parks and 15.44 for mowed parks, with a p-value of 0.01659
+t.test(TOT_RICHNESS~TREATMENT, data=TOT_COUNTS_QUAD, var.equal=FALSE)
 
-t.test(MAY_TOT_ABUND~TREATMENT, data=TOT_COUNTS, var.equal=FALSE)
-# not significant
+# Then, how do mean species richnesses vary between mowed and unmowed parks?
+ggplot(TOT_COUNTS_QUAD, aes(x=TREATMENT, y=MEAN_RICH))+
+  geom_boxplot(stat="boxplot")+
+  geom_point()
+
+t.test(MEAN_RICH~TREATMENT, data=TOT_COUNTS_QUAD, var.equal=FALSE)
+
+# mean abundance and treatment?
+ggplot(TOT_COUNTS_QUAD, aes(x=TREATMENT, y=MEAN_ABUND))+
+  geom_boxplot(stat="boxplot")+
+  geom_point()
+
+t.test(MEAN_ABUND~TREATMENT, data=TOT_COUNTS_QUAD, var.equal=FALSE)
+
+# Boxplot showing difference between treatments over the four months of surveys
+
+quadrat %>% 
+  group_by(TREATMENT, MONTH) %>% 
+  summarise(mean_abund=mean
+
+
 
 
 
